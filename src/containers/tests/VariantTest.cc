3cd606e3d59e4e2af8793bfc29adac2e8c57b6c1
Nick Meyer <nick.j.meyer@gmail.com>
2018-12-16 02:44:01 +0000

    Add extra check.  Still need lots more unit testing.

--- src/containers/tests/VariantTest.cc
+++ src/containers/tests/VariantTest.cc
@@ -15,6 +15,7 @@ struct Y
 TEST(Variant, Default)
 {
   Variant<utility::TaggedType<X, int32_t>, utility::TaggedType<Y, float>> variant{std::in_place_type<X>, 7};
+  EXPECT_EQ(variant.index(), 0);
   visit(variant, [] (auto value)
         {
           constexpr bool is_X = std::is_same_v<std::decay_t<decltype(value)>, int32_t>;
