d89f506c5349e7951fdc9ea6d06fe3faaabbd048
Nick Meyer <nick.j.meyer@gmail.com>
2019-05-23 23:03:55 -0400

    Add partition benchmark.

---
+++ src/algorithm/benchmarks/PartitionBenchmark.cc
@@ -0,0 +1,59 @@
+#include <benchmark/benchmark.h>
+
+#include "src/algorithm/Partition.hh"
+
+#include <random>
+#include <algorithm>
+
+void njm_algorithm_benchmark(benchmark::State& state)
+{
+  auto num_values = state.range(0);
+  std::vector<int32_t> values;
+  values.reserve(num_values);
+  std::mt19937 rng{0};
+  std::uniform_int_distribution<int32_t> dist{-100, 100};
+  for (auto _ : state)
+  {
+    state.PauseTiming();
+    values.clear();
+    for (int32_t i = 0; i < num_values; ++i)
+    {
+      values.push_back(dist(rng));
+    }
+    state.ResumeTiming();
+    njm::algorithm::partition(std::begin(values), std::end(values),
+                              [] (auto x)
+                              {
+                                return x < 20;
+                              });
+  }
+}
+
+void std_algorithm_benchmark(benchmark::State& state)
+{
+  auto num_values = state.range(0);
+  std::vector<int32_t> values;
+  values.reserve(num_values);
+  std::mt19937 rng{0};
+  std::uniform_int_distribution<int32_t> dist{-100, 100};
+  for (auto _ : state)
+  {
+    state.PauseTiming();
+    values.clear();
+    for (int32_t i = 0; i < num_values; ++i)
+    {
+      values.push_back(dist(rng));
+    }
+    state.ResumeTiming();
+    std::partition(std::begin(values), std::end(values),
+                   [] (auto x)
+                   {
+                     return x < 20;
+                   });
+  }
+}
+
+BENCHMARK(njm_algorithm_benchmark)->Range(8, 1 << 24);
+BENCHMARK(std_algorithm_benchmark)->Range(8, 1 << 24);
+
+BENCHMARK_MAIN();
