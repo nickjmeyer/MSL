d2520a60bf043bb88d6101113f5adfc549d77600
Nick Meyer <nick.j.meyer@gmail.com>
2019-09-28 15:14:38 -0400

    Partition benchmark updates.

--- src/algorithm/benchmarks/PartitionBenchmark.cc
+++ src/algorithm/benchmarks/PartitionBenchmark.cc
@@ -12,6 +12,9 @@ void njm_algorithm_benchmark(benchmark::State& state)
   values.reserve(num_values);
   std::mt19937 rng{0};
   std::uniform_int_distribution<int32_t> dist{-100, 100};
+  size_t max_evals{0U};
+  size_t min_evals{0U};
+  --min_evals;
   for (auto _ : state)
   {
     state.PauseTiming();
@@ -21,12 +24,18 @@ void njm_algorithm_benchmark(benchmark::State& state)
       values.push_back(dist(rng));
     }
     state.ResumeTiming();
+    size_t num_evals{0U};
     njm::algorithm::partition(std::begin(values), std::end(values),
-                              [] (auto x)
+                              [&num_evals] (auto x)
                               {
+                                ++num_evals;
                                 return x < 20;
                               });
+    max_evals = std::max(max_evals, num_evals);
+    min_evals = std::min(min_evals, num_evals);
   }
+  state.counters["max_evals"] = max_evals;
+  state.counters["min_evals"] = min_evals;
 }
 
 void std_algorithm_benchmark(benchmark::State& state)
@@ -36,6 +45,9 @@ void std_algorithm_benchmark(benchmark::State& state)
   values.reserve(num_values);
   std::mt19937 rng{0};
   std::uniform_int_distribution<int32_t> dist{-100, 100};
+  size_t max_evals{0U};
+  size_t min_evals{0U};
+  --min_evals;
   for (auto _ : state)
   {
     state.PauseTiming();
@@ -44,13 +56,19 @@ void std_algorithm_benchmark(benchmark::State& state)
     {
       values.push_back(dist(rng));
     }
+    size_t num_evals{0U};
     state.ResumeTiming();
     std::partition(std::begin(values), std::end(values),
-                   [] (auto x)
+                   [&num_evals] (auto x)
                    {
+                     ++num_evals;
                      return x < 20;
                    });
+    max_evals = std::max(max_evals, num_evals);
+    min_evals = std::min(min_evals, num_evals);
   }
+  state.counters["max_evals"] = max_evals;
+  state.counters["min_evals"] = min_evals;
 }
 
 double compute_max(const std::vector<double>&v)
@@ -60,14 +78,14 @@ double compute_max(const std::vector<double>&v)
 
 BENCHMARK(njm_algorithm_benchmark)
 ->Range(8, 1 << 24)
-->Repetitions(10)
-->ReportAggregatesOnly(true)
+// ->Repetitions(10)
+// ->ReportAggregatesOnly(true)
 ->ComputeStatistics("max", compute_max);
 
 BENCHMARK(std_algorithm_benchmark)
 ->Range(8, 1 << 24)
-->Repetitions(10)
-->ReportAggregatesOnly(true)
+// ->Repetitions(10)
+// ->ReportAggregatesOnly(true)
 ->ComputeStatistics("max", compute_max);
 
 BENCHMARK_MAIN();
